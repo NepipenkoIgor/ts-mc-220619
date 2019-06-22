42dd5164bddd3cc3746098132519669793bbcdf5
sfs-inep <igornepipenko@gmail.com>
2019-06-22 12:02:17 +0300

    Feature: add tslint [ref:no-ref]

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", writable: true });
+Object.defineProperty(window, 'MySweetApp', {value: 'v1.0.0', writable: true});
 
-function shipWeight() {
-    const el = document.getElementById('weight');
-    if(!el) {
-        return 0;
+function shipWeight(): number {
+    const el: HTMLElement | null = document.getElementById('weight');
+    if (!el) {
+        return 0
     }
     return parseInt(el.innerHTML);
 }
 
-function sendUpdates(emailAddr: string | string[]) {
-    /**
-     * @param {string} addr
-     */
-    function sendEmail(_req: string) {
+function sendUpdates(emailAddr: string | string[]): void {
+    function sendEmail(_req: string): void {
         if (shipWeight() > 100) {
-            console.log("WARNING: Oversize package");
+            // tslint:disable-next-line:no-console
+            console.log('WARNING: Oversize package');
         }
-        console.log('send package')
+        // tslint:disable-next-line:no-console
+        console.log('send package');
     }
+
     if (Array.isArray(emailAddr)) {
-        emailAddr.forEach((val) => {
+        emailAddr.forEach((val: string) => {
             sendEmail(val.trim());
         });
     } else {
